a83607aa00283a1cc37a163c69463d464eb7be07
Fábio Marques <fabiomarquescbf@gmail.com>
2023-05-21 16:19:03 -0300

    Add files via upload

---
+++ Poo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apache;
+
+/**
+ *
+ * @author fabio
+ */
+public class Poo {
+    
+    // INSTÂNCIA é um objeto criado a partir de uma CLASSE.
+    // CLASSE é um modelo ou esqueleto que define as propriedades e comportamentos de um OBJETO.
+    // Uma CLASSE somente poderá ser utilizada após ser instanciada
+    // Para instânciar objetos -> NomeDaClasse nomeDoObjeto = new NomeDaClasse();
+    
+}
+
+
+
+
+
+    
+
