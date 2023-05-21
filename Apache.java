01a32b2372a2fbf5688fdec9d7d16b9b321e19e5
Fábio Marques <fabiomarquescbf@gmail.com>
2023-05-21 14:56:01 -0300

    Add files via upload

---
+++ Apache.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apache;
+
+/**
+ * aprendendo Java - Oracle https://docs.oracle.com/javase/tutorial/java/index.html
+ * @author fabio
+ */
+
+public class Apache {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
