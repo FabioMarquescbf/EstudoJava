30af6d09edc22c291863affe224b947a40044b2b
Fábio Marques <fabiomarquescbf@gmail.com>
2023-05-21 15:59:26 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apache;
+
+/**
+ *
+ * @author fabio
+ */
+public class Variaveis {
+    
+    // Varáveis de Instância (Não Estáticas). -> exclusivas para cada instância de uma classe
+    
+    //Variáveis de classe (Estáticas). -> qualquer campo declarado com o modificador
+    
+    //Variáveis Locais (Variáveis de metodos) -> exclusivas para cada instância de uma classe
+    
+    //Variáveis de Parâmetros. -> fornecem informações extras a um método
+    
+    //  A classe java.lang.String representa cadeias de caracteres
+    
+    public String primitivos(){ 
+        String tipos =  ("byte ; short ; long ; float ; double ; boolean ; char ; String");
+        return tipos;
+    }
+    
+    
+    /*
+    O termo "variável de instância" é outro nome para ___. -> CAMPO NÃO ESTÁTICO.
+    O termo "variável de classe" é outro nome para ___. -> CAMPO ESTÁTICO.
+    Uma variável local armazena o estado temporário; ele é declarado dentro de um ___. -> MÉTODO.
+    Uma variável declarada dentro do parêntese de abertura e fechamento de uma assinatura de método é chamada de ____. -> PARÂMETRO.
+    Quais são os oito tipos de dados primitivos suportados pela linguagem de programação Java? BYTE, SHORT, INT, LONG, FLOAT, DOUBLE, BOOLEAN,  CHAR..
+    As cadeias de caracteres são representadas pela classe ___. -> JAVA.LANG.sTRING
+    Um ___ é um objeto de contêiner que contém um número fixo de valores de um único tipo. -> MATRIZ
+    */
+    
+    /*
+    Crie um pequeno programa que defina alguns campos.
+    Tente criar alguns nomes de campo ilegais e veja que tipo de erro o compilador produz.
+    Use as regras e convenções de nomenclatura como um guia.
+    Familiarizar-se com erros comuns do compilador facilitará o reconhecimento de bugs em seu código.
+    */
+    
+}  
+
+
